52ec704005887c3019cf67949a1906a50cc8ae43
Serhii Horbenko <shorben@softserveinc.com>
2025-01-10 16:18:46 +0200

    Redesign navigation approach

--- lib/pages/main_pages/main_page.dart
+++ lib/pages/main_pages/main_page.dart
@@ -8,17 +8,14 @@ import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
   MainPage({super.key});
+
   final String? baseUrl = dotenv.env["BASE_URL"];
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        if (constraints.maxWidth < constraints.maxHeight) {
-          return const MobileMainPage();
-        } else {
-          return const DesktopMainPage();
-        }
+        return const DesktopMainPage();
       },
     );
   }
@@ -32,6 +29,8 @@ class DesktopMainPage extends StatefulWidget {
 }
 
 class _DesktopMainPageState extends State<DesktopMainPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
   bool isKeyboardInput = false;
   String selectedEventType = "dating";
   int selectedNumberOfPeople = 1;
@@ -56,152 +55,157 @@ class _DesktopMainPageState extends State<DesktopMainPage> {
       {"id": "hang_out", "label": "test_page.hang_out".tr()},
     ];
     return Scaffold(
-      body: Stack(
-        children: [
-          const NavigationPanel(),
-          Positioned(
-            right: 0,
-            child: IconButton(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => _scaffoldKey.currentState?.openDrawer(),
+          icon: Icon(Icons.menu),
+        ),
+      ),
+      drawer: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            const NavigationPanel(),
+            Spacer(),
+            IconButton(
               icon: Icon(Icons.account_circle_outlined,
                   color: Theme.of(context).iconTheme.color),
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const UserProfilePage()),
+                  MaterialPageRoute(
+                      builder: (context) => const UserProfilePage()),
                 );
               },
               iconSize: 45,
             ),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "test_page.greeting".tr(),
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "test_page.greeting".tr(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Theme(
+                  data: Theme.of(context).copyWith(
+                    canvasColor: Colors.white,
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      value: selectedEventType,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedEventType = newValue!;
+                        });
+                      },
+                      items:
+                          eventTypesData.map<DropdownMenuItem<String>>((item) {
+                        return DropdownMenuItem<String>(
+                          value: item["id"],
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Text(item["label"]!),
+                          ),
+                        );
+                      }).toList(),
+                      icon: Icon(Icons.arrow_drop_down),
+                      isExpanded: true,
+                      dropdownColor: Colors.white,
+                      style: const TextStyle(color: Colors.black),
+                      borderRadius: BorderRadius.circular(15),
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  Container(
-                    width: 400,
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: Theme(
-                      data: Theme.of(context).copyWith(
-                        canvasColor: Colors.white,
-                      ),
-                      child: DropdownButtonHideUnderline(
-                        child: DropdownButton<String>(
-                          value: selectedEventType,
-                          onChanged: (String? newValue) {
-                            setState(() {
-                              selectedEventType = newValue!;
-                            });
-                          },
-                          items: eventTypesData.map<DropdownMenuItem<String>>((item) {
-                            return DropdownMenuItem<String>(
-                              value: item["id"],
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                                child: Text(item["label"]!),
-                              ),
-                            );
-                          }).toList(),
-                          icon: Icon(Icons.arrow_drop_down),
-                          isExpanded: true,
-                          dropdownColor: Colors.white,
-                          style: const TextStyle(color: Colors.black),
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                    ),
+                ),
+                const SizedBox(height: 10),
+                CustomDropdown(
+                  label: "test_page.number_of_people".tr(),
+                  items: List.generate(100, (index) => (index + 1).toString()),
+                  selectedValue: selectedNumberOfPeople.toString(),
+                  onChanged: (value) {
+                    setState(() {
+                      selectedNumberOfPeople = int.parse(value!);
+                    });
+                  },
+                ),
+                const SizedBox(height: 10),
+                CustomDropdown(
+                  label: "test_page.budget".tr(),
+                  items:
+                      List.generate(10000, (index) => (index * 10).toString()),
+                  selectedValue: selectedBudget.toString(),
+                  onChanged: (value) {
+                    setState(() {
+                      selectedBudget = int.parse(value!);
+                    });
+                  },
+                ),
+                const SizedBox(height: 10),
+                TimePickerWidget(
+                  label: "test_page.duration".tr(),
+                  selectedTime: selectedDuration,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedDuration = value!;
+                    });
+                  },
+                ),
+                const SizedBox(height: 20),
+                FindButton(
+                  data: {
+                    'userId': 1,
+                    'eventType': selectedEventType,
+                    'peopleCount': selectedNumberOfPeople,
+                    'eventTime': selectedDuration.format(context),
+                    'costTier': selectedBudget,
+                  },
+                  sendDataToServer: sendDataToServer,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "test_page.or".tr(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(height: 10),
-                  CustomDropdown(
-                    label: "test_page.number_of_people".tr(),
-                    items: List.generate(100, (index) => (index + 1).toString()),
-                    selectedValue: selectedNumberOfPeople.toString(),
-                    onChanged: (value) {
-                      setState(() {
-                        selectedNumberOfPeople = int.parse(value!);
-                      });
-                    },
+                ),
+                const SizedBox(height: 20),
+                IconButton(
+                  icon: Icon(Icons.mic_none_outlined,
+                      color: Theme.of(context).iconTheme.color),
+                  onPressed: () {
+                    /*Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ...()),
+                    );*/
+                  },
+                  iconSize: 100,
+                ),
+                Text(
+                  "test_page.voice".tr(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(height: 10),
-                  CustomDropdown(
-                    label: "test_page.budget".tr(),
-                    items: List.generate(10000, (index) => (index * 10).toString()),
-                    selectedValue: selectedBudget.toString(),
-                    onChanged: (value) {
-                      setState(() {
-                        selectedBudget = int.parse(value!);
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 10),
-                  TimePickerWidget(
-                    label: "test_page.duration".tr(),
-                    selectedTime: selectedDuration,
-                    onChanged: (value) {
-                      setState(() {
-                        selectedDuration = value!;
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  FindButton(
-                    data: {
-                      'userId': 1,
-                      'eventType': selectedEventType,
-                      'peopleCount': selectedNumberOfPeople,
-                      'eventTime': selectedDuration.format(context),
-                      'costTier': selectedBudget,
-                    },
-                    sendDataToServer: sendDataToServer,
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    "test_page.or".tr(),
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  IconButton(
-                    icon: Icon(Icons.mic_none_outlined, color: Theme.of(context).iconTheme.color),
-                    onPressed: () {
-                      /*Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const ...()),
-                      );*/
-                    },
-                    iconSize: 100,
-                  ),
-                  Text(
-                    "test_page.voice".tr(),
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
@@ -223,7 +227,8 @@ class _MobileMainPageState extends State<MobileMainPage> {
           Positioned(
             left: 0,
             child: IconButton(
-              icon: Icon(Icons.arrow_back, color: Theme.of(context).iconTheme.color),
+              icon: Icon(Icons.arrow_back,
+                  color: Theme.of(context).iconTheme.color),
               onPressed: () {
                 Navigator.pop(context);
               },
